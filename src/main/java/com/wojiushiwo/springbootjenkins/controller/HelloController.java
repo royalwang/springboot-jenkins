65b7ce4f078f2fc9cab2f53953b5f2f149b6aba8
myk <yinkun.meng@petkit.com>
2020-06-03 13:40:59 +0800

    add pay

--- src/main/java/com/wojiushiwo/springbootjenkins/controller/HelloController.java
+++ src/main/java/com/wojiushiwo/springbootjenkins/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.wojiushiwo.springbootjenkins.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by myk
  * 2020/5/13 下午5:35
@@ -11,7 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/pay")
+    public void pay(HttpServletRequest request, HttpServletRequest response) {
+        String agent = request.getHeader("User-Agent").toLowerCase();
+        System.out.println("响应头的类型：" + agent);
+        if (agent.indexOf("micromessenger") > 0) {
+            System.out.println("微信支付");
+        } else if (agent.indexOf("alipayclient") > 0) {
+            System.out.println("阿里支付");
+        }
     }
 }
